d9b26a9dea96c167a28f71f1f59f2386c94524ce
hyunholee <dionme2622@gmail.com>
2023-09-29 18:00:01 +0900

    마우스 좌클릭을 하였을 때 손 모양이 화면에 남게 함

--- Drill06.py
+++ Drill06.py
@@ -6,19 +6,34 @@ open_canvas(WIDTH, HEIGHT)
 Hand = load_image("hand_arrow.png")
 Character = load_image("run_animation.png")
 Background = load_image("TUK_GROUND.png")
+Point_x = []
+Point_y = []
+
+
 def Handle_events():
-    global running, mx, my
+    global running, mx, my, Click
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             mx, my = event.x, HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                Point_x.append(mx)
+                Point_y.append(my)
 
+                Click = True
+        elif event.type == SDL_MOUSEBUTTONUP:
+            Click = False
 def Draw_hand(x, y):
     Hand.draw(x, y)
+    for i in range(0, len(Point_x)):
+        Hand.draw(Point_x[i], Point_y[i])
+    # if Click:
+    #     Hand.draw()
 
-running = True
+running, Click = True, False
 mx, my = 0, 0
 while running:
     Background.draw(WIDTH // 2, HEIGHT // 2)
