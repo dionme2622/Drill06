fd61e85efa52b306952a232496a547fc8d18e536
hyunholee <dionme2622@gmail.com>
2023-09-29 17:29:43 +0900

    Draw_hand 함수 선언 완료

--- Drill06.py
+++ Drill06.py
@@ -7,18 +7,24 @@ Hand = load_image("hand_arrow.png")
 Character = load_image("run_animation.png")
 Background = load_image("TUK_GROUND.png")
 def Handle_events():
-    global running
+    global running, mx, my
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_MOUSEMOTION:
+            mx, my = event.x, HEIGHT - 1 - event.y
 
+def Draw_hand(x, y):
+    Hand.draw(x, y)
 
 running = True
+mx, my = 0, 0
 while running:
     Background.draw(WIDTH // 2, HEIGHT // 2)
-    Draw_hand(x, y)
+    Draw_hand(mx, my)
     update_canvas()
     Handle_events()
+    hide_cursor()
 
 close_canvas()
